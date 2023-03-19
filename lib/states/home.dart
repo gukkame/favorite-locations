c68125a2374f29dea7469edc4523e9bff9bb07fc
gukkame <guka245@gmail.com>
2023-03-19 15:25:23 +0200

    get location of searched place, display details of place, change camera view to new place

--- lib/states/home.dart
+++ lib/states/home.dart
@@ -1,10 +1,14 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_google_places_hoc081098/flutter_google_places_hoc081098.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
+import 'package:google_maps_webservice/places.dart';
+import 'package:google_api_headers/google_api_headers.dart';
 import '../widgets/scaffold.dart';
+
+const kGoogleApiKey = 'AIzaSyCp5EfjwJY4StgxAWjIIKim2tJ0N8L2TUw';
 
 class MyHomePage extends StatefulWidget {
   final String title;
@@ -15,11 +19,32 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final _places = GoogleMapsPlaces(apiKey: kGoogleApiKey);
+  Future<void> _onTapMap(LatLng position) async {
+    final result = await _places.searchNearbyWithRadius(
+      Location(lat: position.latitude, lng: position.longitude),
+      1000,
+    );
+
+    if (result.status == "OK" && result.results.isNotEmpty) {
+      final place = result.results.first;
+      final name = place.name;
+      final description = place.formattedAddress;
+
+      // TODO: Display the name and description on the screen
+    }
+  }
+
+/////////////////////////////////////
+
   late GoogleMapController mapController;
   late LatLng currentLocation;
 
   void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
+    // mapController = controller;
+    setState(() {
+      mapController = controller;
+    });
   }
 
   Future<void> _getCurrentLocation() async {
@@ -45,32 +70,99 @@ class _MyHomePageState extends State<MyHomePage> {
     _getCurrentLocation();
   }
 
+  Mode _mode = Mode.overlay;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Container(
-        child: SafeArea(
-          child: GoogleMap(
-            initialCameraPosition: CameraPosition(
-              target: currentLocation,
-              zoom: 12,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: GoogleMap(
+                initialCameraPosition: CameraPosition(
+                  target: currentLocation,
+                  zoom: 12,
+                ),
+                markers: Set.from([
+                  Marker(
+                    markerId: MarkerId("current-location"),
+                    position: currentLocation,
+                    infoWindow: InfoWindow(title: "You are here"),
+                  )
+                ]),
+                mapType: MapType.normal,
+                myLocationEnabled: true,
+                compassEnabled: true,
+                onMapCreated: _onMapCreated,
+                onTap: _onTapMap,
+              ),
             ),
-            markers: Set.from([
-              Marker(
-                markerId: MarkerId("current-location"),
-                position: currentLocation ?? const LatLng(37.77483, -122.41942),
-                infoWindow: InfoWindow(title: "You are here"),
-              )
-            ]),
-            mapType: MapType.normal,
-            myLocationEnabled: true,
-            compassEnabled: true,
-            onMapCreated: _onMapCreated,
-          ),
+            const SizedBox(height: 12),
+            ElevatedButton(
+              onPressed: _handlePressButton,
+              child: const Text('Search places'),
+            ),
+            const SizedBox(height: 12),
+          ],
         ),
+      ),
+    );
+  }
+
+  Future<void> _handlePressButton() async {
+    void onError(PlacesAutocompleteResponse response) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(response.errorMessage ?? 'Unknown error'),
+        ),
+      );
+    }
+
+    final p = await PlacesAutocomplete.show(
+      context: context,
+      apiKey: kGoogleApiKey,
+      onError: onError,
+      mode: _mode,
+      language: 'eng',
+      components: [Component(Component.country, 'ee')],
+      resultTextStyle: Theme.of(context).textTheme.subtitle1,
+    );
+
+    await displayPrediction(p, ScaffoldMessenger.of(context));
+  }
+
+  Future<void> displayPrediction(
+      Prediction? p, ScaffoldMessengerState messengerState) async {
+    if (p == null) {
+      return;
+    }
+    final _places = GoogleMapsPlaces(
+      apiKey: kGoogleApiKey,
+      apiHeaders: await const GoogleApiHeaders().getHeaders(),
+    );
+
+    final detail = await _places.getDetailsByPlaceId(p.placeId!);
+    final geometry = detail.result.geometry!;
+    final lat = geometry.location.lat;
+    final lng = geometry.location.lng;
+
+    setState(() {
+      currentLocation = LatLng(lat, lng);
+    });
+    mapController.animateCamera(CameraUpdate.newCameraPosition(
+      CameraPosition(
+        target: LatLng(currentLocation.latitude, currentLocation.longitude),
+        zoom: 15.0,
+      ),
+    ));
+
+    messengerState.showSnackBar(
+      SnackBar(
+        content: Text('${p.description} - $lat/$lng'),
       ),
     );
   }
